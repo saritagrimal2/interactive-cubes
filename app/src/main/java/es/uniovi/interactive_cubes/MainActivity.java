3d7aee36a32198c99157eed199eec6e017be191b
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-12-09 15:29:48 +0100

    user help

--- app/src/main/java/es/uniovi/interactive_cubes/MainActivity.java
+++ app/src/main/java/es/uniovi/interactive_cubes/MainActivity.java
@@ -1,6 +1,7 @@
 package es.uniovi.interactive_cubes;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -20,6 +21,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -84,14 +87,25 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.menubtn) {
-        //    return true;
+
+            final Dialog help = new Dialog(this);
+            help.setContentView(R.layout.help);
+            help.show();
+
+            Button btnAccept = help.findViewById(R.id.btnAcept);
+            btnAccept.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    help.dismiss();
+                }
+            });
+
+
         }
 
         return super.onOptionsItemSelected(item);
